87f4cdaa05716433eff41c76871575ca1a074df0
Bo Wang <bowang@Bos-MacBook-Pro-2.local>
2015-06-07 19:21:49 +1000

    Initial Commit

---
+++ OpenHome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenHome
+//
+//  Created by Bo Wang on 7/06/2015.
+//  Copyright (c) 2015 Bo Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OpenHome/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OpenHome
+//
+//  Created by Bo Wang on 7/06/2015.
+//  Copyright (c) 2015 Bo Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
