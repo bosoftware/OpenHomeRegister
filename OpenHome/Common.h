930d1f3d68a20b6908f23583cee52840e0fe13d9
Bo Wang <okwangbo@gmail.com>
2016-07-22 22:14:35 +1000

    no message

--- OpenHome/Common.h
+++ OpenHome/Common.h
@@ -9,5 +9,13 @@
 #ifndef OpenHome_Common_h
 #define OpenHome_Common_h
 
-
+#define pageSize 50
+#define textQuestion 1
+#define multipleChoiceQuestion 2
+#define mandatoryQuestion 1
+#define optionalQuestion 2
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import <Parse/Parse.h>
+#define upgrade_key @"youupgradekey"
 #endif
